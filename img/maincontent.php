876f1b4e437e1b7eac5b891ce525436cbf35c52c
GabrooTech <87397156+GabrooTech@users.noreply.github.com>
2023-09-20 19:37:01 +0400

    Add files via upload

---
+++ img/maincontent.php
@@ -0,0 +1,105 @@
+<?php
+?>
+
+<div class="main_container">
+    <div class="main_list">
+        <div class="item_1 main_list_item">
+            <a href="#" class="pc_image_link"></a>
+            <img src="img\pc.webp" alt="Check your internet connection" class="list_img">   
+            <div class="main_list_name main_list_name_color_pc">PC</div> 
+        </div>
+        <div class="item_2 main_list_item">
+            <a href="#"></a>
+            <img src="img\servers.webp" alt="Check your internet connection" class="list_img">   
+            <div class="main_list_name main_list_name_color_server">SERVERS</div>
+        </div>
+        <div class="item_3 main_list_item">
+            <a href="#"></a>
+            <img src="img\accessories.webp" alt="Check your internet connection" class="list_img">   
+            <div class="main_list_name main_list_name_color_accs">ACCESSORIES</div>
+        </div>
+        <div class="item_4 main_list_item">
+            <a href="#"></a>
+            <img src="img\office.webp" alt="Check your internet connection" class="list_img">   
+            <div class="main_list_name main_list_name_color_office">OFFICE</div>
+        </div>
+        <div class="item_5 main_list_item">
+            <a href="#"></a>
+            <img src="img\smarthome.webp" alt="Check your internet connection" class="list_img">   
+            <div class="main_list_name main_list_name_color_smhome">SMART HOME</div>
+        </div>
+        <div class="item_6 main_list_item">
+            <a href="#"></a>
+            <img src="img\mobile.webp" alt="Check your internet connection" class="list_img">   
+            <div class="main_list_name main_list_name_color_md">MOBILE DEVICE </div>
+        </div>
+    </div>
+    <section class="card_section">
+        <div class="card">
+            <div class="special_deal">50%</div>
+            <div class="card_img_box">
+                <img src="img/test.png" alt="check your internet connection">
+            </div>
+            <div class="content_box">
+                <h3 class="product_name">Wireless Headphones</h3>
+                <h2 class="product_price">₾329</h2>
+                <a href="#" class="add_to_cart add_button"><span>Check out</span></a>
+            </div>
+        </div>
+        <div class="card card_2">
+            <div class="card_img_box">
+                <img src="img/gmouse.png" alt="check your internet connection">
+            </div>
+            <div class="content_box">
+                <h3 class="product_name">Wireless Headphones</h3>
+                <h2 class="product_price">₾280</h2>
+                <a href="#" class="add_to_cart add_button"><span>Check out</span></a>
+                <div></div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="special_deal">50%</div>
+            <div class="card_img_box">
+                <img src="img/test.png" alt="check your internet connection">
+            </div>
+            <div class="content_box">
+                <h3 class="product_name">Wireless Headphones</h3>
+                <h2 class="product_price">₾329</h2>
+                <a href="#" class="add_to_cart add_button"><span>Check out</span></a>
+            </div>
+        </div>
+        <div class="card card_2">
+            <div class="card_img_box">
+                <img src="img/gmouse.png" alt="check your internet connection">
+            </div>
+            <div class="content_box">
+                <h3 class="product_name">Wireless Headphones</h3>
+                <h2 class="product_price">₾280</h2>
+                <a href="#" class="add_to_cart add_button"><span>Check out</span></a>
+                <div></div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="special_deal">50%</div>
+            <div class="card_img_box">
+                <img src="img/test.png" alt="check your internet connection">
+            </div>
+            <div class="content_box">
+                <h3 class="product_name">Wireless Headphones</h3>
+                <h2 class="product_price">₾329</h2>
+                <a href="#" class="add_to_cart add_button"><span>Check out</span></a>
+            </div>
+        </div>
+        <div class="card card_2">
+            <div class="card_img_box">
+                <img src="img/gmouse.png" alt="check your internet connection">
+            </div>
+            <div class="content_box">
+                <h3 class="product_name">Wireless Headphones</h3>
+                <h2 class="product_price">₾280</h2>
+                <a href="#" class="add_to_cart add_button"><span>Check out</span></a>
+                <div></div>
+            </div>
+        </div>
+    </section>
+</div>
