091614971ee45d2254823ce20ff12a97f634dadf
GabrooTech <87397156+GabrooTech@users.noreply.github.com>
2023-09-28 15:36:05 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -65,14 +65,14 @@ function brandChanger(){
 var brandTimer = setInterval(brandChanger, 3000)
 brand.addEventListener("mouseover", function(){ clearInterval(brandTimer)});
 brand.addEventListener("mouseout", function(){ brandTimer = setInterval(brandChanger, 3000)})
-var brandlocatorswitch = ["0px","-280px","-600px","-850px","-1200px","-1485px"]
-var brandlocatorReverseSwitch = ["-1485px","-1200px","-850px","-600px","-280px","0px"]
+var brandlocatorswitch = ["0px","-280px","-600px","-850px","-1200px","-1485px","-1780px"]
+var brandlocatorReverseSwitch = ["-1485px", "-1485px","-1200px","-910px","-600px","-280px","0px"]
 var saleBackBtn = document.querySelector(".sale_back")
 var saleNextBtn = document.querySelector(".sale_next")
 var saleArrowBox = document.querySelector(".sale_view_list")
 var arrowCounter = 0
 saleNextBtn.addEventListener("click", () => {
-  if(arrowCounter < 7){
+  if(arrowCounter < 5){
     arrowCounter++;
   }else{
     arrowCounter = 0;
@@ -80,7 +80,7 @@ saleNextBtn.addEventListener("click", () => {
   box.style.translate = locator[arrowCounter];
 })
 saleBackBtn.addEventListener("click", () => {
-  if(arrowCounter < 7){
+  if(arrowCounter < 5){
     arrowCounter++;
   }else{
     arrowCounter = 0;
@@ -94,7 +94,7 @@ var trendingNextBtn = document.querySelector(".trending_next")
 var trendingArrowBox = document.querySelector(".trending_view_list")
 var arrowCounterTrending = 0
 trendingNextBtn.addEventListener("click", () => {
-  if(arrowCounterTrending < 7){
+  if(arrowCounterTrending < 5){
     arrowCounterTrending++;
   }else{
     arrowCounterTrending = 0;
@@ -102,7 +102,7 @@ trendingNextBtn.addEventListener("click", () => {
   trending_box.style.translate = locator[arrowCounterTrending];
 })
 trendingBackBtn.addEventListener("click", () => {
-  if(arrowCounterTrending < 7){
+  if(arrowCounterTrending < 5){
     arrowCounterTrending++;
   }else{
     arrowCounterTrending = 0;
@@ -116,20 +116,83 @@ var brandNextBtn = document.querySelector(".brand_next")
 var brandArrowBox = document.querySelector(".brands_view_list")
 var arrowCounterbrand = 0
 brandNextBtn.addEventListener("click", () => {
-  if(arrowCounterbrand < 7){
-    arrowCounterbrand++;
-  }else{
-    arrowCounterbrand = 0;
+  if(maxWidth < 1520 && maxWidth > 1080){
+    if(arrowCounterbrand < 7){
+      arrowCounterbrand++;
+    }else{
+      arrowCounterbrand = 0;
+    }
+    brand.style.translate = brandlocatorswitchnotPersonal[arrowCounterbrand];
+  }else if(maxWidth == 1080){
+    if(arrowCounterbrand < 8){
+      arrowCounterbrand++;
+    }else{
+      arrowCounterbrand = 0;
+    }
+    brand.style.translate = brandlocatorswitchnotPersonalsmall[arrowCounterbrand];
+  }else if(maxWidth > 1750){
+    if(arrowCounterbrand < 5){
+      arrowCounterbrand++;
+    }else{
+      arrowCounterbrand = 0;
+    }
+    brand.style.translate = brandlocatorswitch[arrowCounterbrand];
+  }else if(maxWidth <= 1750 && maxWidth > 1520){
+    if(arrowCounterbrand < 6){
+      arrowCounterbrand++;
+    }else{
+      arrowCounterbrand = 0;
+    }
+    brand.style.translate = brandlocatorSwitchMiddle[arrowCounterbrand];
   }
-  brand.style.translate = brandlocatorswitch[arrowCounterbrand];
+  console.log(arrowCounterbrand)
 })
 brandBackBtn.addEventListener("click", () => {
-  if(arrowCounterbrand < 7){
-    arrowCounterbrand++;
-  }else{
-    arrowCounterbrand = 0;
+  if(maxWidth <= 1520 && maxWidth > 1080){
+    if(arrowCounterbrand < 8){
+      arrowCounterbrand++;
+    }else{
+      arrowCounterbrand = 0;
+    }
+    brand.style.translate = brandlocatorReverseSwitchnotPersonal[arrowCounterbrand];
+  }else if(maxWidth <= 1080){
+    if(arrowCounterbrand < 8){
+      arrowCounterbrand++;
+    }else{
+      arrowCounterbrand = 0;
+    }
+    brand.style.translate = brandlocatorReverseSwitchnotPersonalsmall[arrowCounterbrand];
+  }else if(maxWidth > 1520){
+    if(arrowCounterbrand < 6){
+      arrowCounterbrand++;
+    }else{
+      arrowCounterbrand = 0;
+    }
+    brand.style.translate = brandlocatorReverseSwitch[arrowCounterbrand];
+  }else if(maxWidth <= 1750 && maxWidth > 1520){
+    if(arrowCounterbrand < 6){
+      arrowCounterbrand++;
+    }else{
+      arrowCounterbrand = 0;
+    }
+    brand.style.translate = brandlocatorReverseSwitchMiddle[arrowCounterbrand];
   }
-  brand.style.translate = brandlocatorReverseSwitch[arrowCounterbrand];
+  console.log(arrowCounterbrand)
 })
 brandArrowBox.addEventListener("mouseover", function(){ clearInterval(brandTimer)});
 brandArrowBox.addEventListener("mouseout", function(){ brandTimer = setInterval(brandChanger, 5000);});
+// var notPersonal = window.matchMedia("(max-width: 1520px)")
+// var notPersonalsmall = window.matchMedia("(max-width: 1080px)")
+var brandlocatorswitchnotPersonal = ["0px","-280px","-600px","-850px","-1200px","-1485px","-1750px","-2100px"]
+var brandlocatorReverseSwitchnotPersonal = ["-1800px","-2100px","-1800px","-1520px","-1180px","-880px","-580px","-280px","35px"]
+var brandlocatorswitchnotPersonalsmall = ["0px","-280px","-600px","-850px","-1200px","-1485px","-1750px","-2100px", "-2350px"]
+var brandlocatorReverseSwitchnotPersonalsmall = ["-2300px","-2300px","-2050px","-1705px","-1400px","-1100px","-800px","-500px","-300px"]
+var brandlocatorSwitchMiddle = ["0px","-280px","-600px","-850px","-1200px","-1485px","-1750px"]
+var brandlocatorReverseSwitchMiddle = ["-1750px","-1685px","-1200px","-850px","-600px","-280px","0px"]
+var maxWidth = Math.max(
+  document.body.scrollWidth,
+  document.documentElement.scrollWidth,
+  document.body.offsetWidth,
+  document.documentElement.offsetWidth,
+  document.documentElement.clientWidth
+);
